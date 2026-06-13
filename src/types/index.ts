d5d3ec842590e0e6d3cf10536cf17c018d1b384e
weerayosong <weerayosong@outlook.com>
2026-06-13 15:14:23 +0700

    define core ts type, interface, and union

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export type ForceSide = "Jedi" | "Sith";
+export type EraType =
+    | "Old Republic"
+    | "High Republic"
+    | "Prequel"
+    | "Original"
+    | "Sequel"
+    | "All";
+export type SortOption = "mCountDesc" | "mCountAsc" | "nameAsc";
+
+export interface BaseEntity {
+    id: string;
+    name: string;
+    era: EraType;
+    combatStyle: string;
+    midichlorian: number;
+}
+
+export interface JediEntity extends BaseEntity {
+    side: "Jedi";
+    jediRole: string;
+    lightsaberColor: string;
+}
+
+export interface SithEntity extends BaseEntity {
+    side: "Sith";
+    darthTitle: string;
+    sithRole: string;
+    corruptionLevel: number;
+}
+
+export type ForceEntity = JediEntity | SithEntity;
